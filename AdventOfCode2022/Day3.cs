a6a82a1722a4640e56dceb68440a20ef1cd9e24e
Erik <erikvsoderberg@gmail.com>
2022-12-03 10:27:50 +0100

    Solved Day3, Part 1

---
+++ AdventOfCode2022/Day3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Day3 : AoCDay
+    {
+        public static int ToPriority(char letter)
+        {
+            if ('a' <= letter && letter <= 'z') return letter - 'a' + 1;
+            else if ('A' <= letter && letter <= 'Z') return letter - 'A' + 27;
+            else return 0;
+        }
+
+
+        public static int CalculateRucksackSharedItemPriority(string rucksack)
+        {
+
+            HashSet<char> Compartment1 = new();
+            char commonLetter = '\0';
+            int compartmentSize = rucksack.Length / 2; // Length guaranteed to be even.
+            for (int i = 0; i < compartmentSize; i++)
+            {
+                Compartment1.Add(rucksack[i]);
+            }
+            for (int i = compartmentSize; i < rucksack.Length; i++)
+            {
+                if (Compartment1.Contains(rucksack[i])) { commonLetter = rucksack[i]; break; }
+            }
+            return ToPriority(commonLetter);
+        }
+
+        public static string ExecutePart1(List<string> input)
+        {
+            int compartmentSums = 0;
+            foreach(string line in input)
+            {
+                compartmentSums += CalculateRucksackSharedItemPriority(line);
+            }
+            return compartmentSums.ToString();
+        }
+
+        public static string ExecutePart2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2022_Tests/Day3Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022_Tests
+{
+    [TestClass]
+    public class Day3Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual("157", Day3.ExecutePart1(AoCFile.ReadTestInput(3)));
+        }
+    }
+}
