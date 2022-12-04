acae4b6d9733d0817a93d54c5a892cf080176672
Erik <erikvsoderberg@gmail.com>
2022-12-04 10:30:42 +0100

    Solved Day 4, Part 1

---
+++ AdventOfCode2022/Day4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Day4 : AoCDay
+    {
+        private static Regex pairExtractor = new Regex(@"(?<s1>\d+)-(?<e1>\d+),(?<s2>\d+)-(?<e2>\d+)");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s1">Start range 1</param>
+        /// <param name="e1">End range 1, must be LEQ than s1</param>
+        /// <param name="s2">Start range 2</param>
+        /// <param name="e2">End range 2, must be LEQ than s2</param>
+        /// <returns></returns>
+        public static bool IsRangeContainedByOtherRange(int s1, int e1, int s2, int e2)
+        {
+            if (s1 <= s2 && e2 <= e1) // Range 2 is contained by range 1  s1 - s2  e2
+            {
+                return true;
+            }
+            else if (s2 <= s1 && e1 <= e2) // Range 1 is contained by range 2
+            {
+                return true;
+            }
+            else return false;
+        }
+        public static string ExecutePart1(List<string> input)
+        {
+            int containedRanged = 0;
+            foreach (var line in input)
+            {
+                var match = pairExtractor.Match(line);
+                if (match.Success)
+                {
+                    int s1 = int.Parse(match.Groups["s1"].Value);
+                    int e1 = int.Parse(match.Groups["e1"].Value);
+                    int s2 = int.Parse(match.Groups["s2"].Value);
+                    int e2 = int.Parse(match.Groups["e2"].Value);
+                    if(IsRangeContainedByOtherRange(s1,e1, s2, e2))
+                    {
+                        containedRanged++;
+                    }
+                }
+            }
+            return containedRanged.ToString();
+        }
+
+        public static string ExecutePart2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2022_Tests/Day4Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022_Tests
+{
+    [TestClass]
+    public class Day4Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual("2", Day4.ExecutePart1(AoCFile.ReadTestInput(4)));
+        }
+    }
+}
