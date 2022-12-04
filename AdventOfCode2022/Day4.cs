528a8b9ca4ece22a9e9736eb2ce8001b5e51f1cc
Erik <erikvsoderberg@gmail.com>
2022-12-04 10:40:17 +0100

    Solved Day 4, Part 2

--- AdventOfCode2022/Day4.cs
+++ AdventOfCode2022/Day4.cs
@@ -31,6 +31,15 @@ namespace AdventOfCode2022
             }
             else return false;
         }
+
+        public static bool IsRangeOverlappedByOtherRange(int s1, int e1, int s2, int e2)
+        {
+            // The ranges are only non-overlapping if the end of range 1 is before start of range 2
+            // or if the end of range 2 is before the start of range 1.
+            if (e1 < s2 || e2 < s1) return false;
+            else return true;
+        }
+
         public static string ExecutePart1(List<string> input)
         {
             int containedRanged = 0;
@@ -54,7 +63,23 @@ namespace AdventOfCode2022
 
         public static string ExecutePart2(List<string> input)
         {
-            throw new NotImplementedException();
+            int overlappedRanges = 0;
+            foreach (var line in input)
+            {
+                var match = pairExtractor.Match(line);
+                if (match.Success)
+                {
+                    int s1 = int.Parse(match.Groups["s1"].Value);
+                    int e1 = int.Parse(match.Groups["e1"].Value);
+                    int s2 = int.Parse(match.Groups["s2"].Value);
+                    int e2 = int.Parse(match.Groups["e2"].Value);
+                    if (IsRangeOverlappedByOtherRange(s1, e1, s2, e2))
+                    {
+                        overlappedRanges++;
+                    }
+                }
+            }
+            return overlappedRanges.ToString();
         }
     }
 }
--- AdventOfCode2022_Tests/Day4Test.cs
+++ AdventOfCode2022_Tests/Day4Test.cs
@@ -15,5 +15,18 @@ namespace AdventOfCode2022_Tests
         {
             Assert.AreEqual("2", Day4.ExecutePart1(AoCFile.ReadTestInput(4)));
         }
+        [TestMethod]
+        public void TestPart2()
+        {
+            Assert.AreEqual("4", Day4.ExecutePart2(AoCFile.ReadTestInput(4)));
+        }
+        [TestMethod]
+        public void TestOverlappingRange()
+        {
+            Assert.IsTrue(Day4.IsRangeOverlappedByOtherRange(5, 7, 7, 9));
+            Assert.IsTrue(Day4.IsRangeOverlappedByOtherRange(2, 8, 3, 7));
+            Assert.IsTrue(Day4.IsRangeOverlappedByOtherRange(6, 6, 4, 6));
+            Assert.IsTrue(Day4.IsRangeOverlappedByOtherRange(2, 6, 4, 8));
+        }
     }
 }
