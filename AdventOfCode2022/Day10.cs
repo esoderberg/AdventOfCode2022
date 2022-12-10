b6fa64efde4388909cd3a62d5a6b1bbcd6619715
Erik <erikvsoderberg@gmail.com>
2022-12-10 11:11:49 +0100

    Add Day 10, Part 1

---
+++ AdventOfCode2022/Day10.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventOfCode2022.Day10;
+
+namespace AdventOfCode2022
+{
+    public class Day10 : AoCDay
+    {
+
+        public record Instruction(int TimeToExec)
+        {
+            public static Instruction Parse(string s)
+            {
+                if (s == "noop") return new Noop();
+                else
+                {
+                    var parts = s.Split(" ");
+                    if (parts[0] == "addx") return new Addx(int.Parse(parts[1]));
+                }
+                throw new ArgumentException("Invalid instruction");
+            }
+        };
+        
+        public record Addx(int V) : Instruction(1);
+        public record Noop() : Instruction(0);
+
+        public class Processor
+        {
+            private int registerX = 1;
+            public int Cycle { get; private set; } = 1;
+            public int RegisterX => registerX;
+            private List<Instruction> activeInstructions = new();
+            private Queue<Instruction> waitingInstructions = new();
+            public void Process()
+            {
+                if(activeInstructions.Count == 0 && waitingInstructions.Count != 0)
+                {
+                    activeInstructions.Add(waitingInstructions.Dequeue());
+                }
+                List<Instruction> newActiveInstructions = new();
+                foreach (var instruction in activeInstructions)
+                {
+                    if (instruction.TimeToExec == 0)
+                    {
+                        switch (instruction)
+                        {
+                            case Addx addx: registerX += addx.V; break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        newActiveInstructions.Add(instruction with { TimeToExec = instruction.TimeToExec - 1 });
+                    }
+                }
+                activeInstructions = newActiveInstructions;
+                Cycle++;
+            }
+            public void Load(Instruction instruction)
+            {
+                waitingInstructions.Enqueue(instruction);
+            }
+        }
+
+        public static string ExecutePart1(List<string> input)
+        {
+            var instructions = input.Select(Instruction.Parse).ToList();
+            Processor processor = new Processor();
+            List<(int,int)> CycleRegXValues = new();
+            for (int i = 0; i < 220; i++)
+            {
+                if (i < instructions.Count)
+                    processor.Load(instructions[i]);
+
+                if (processor.Cycle == 20 || processor.Cycle-CycleRegXValues.Count*40 == 20)
+                    CycleRegXValues.Add((processor.Cycle, processor.RegisterX));
+                processor.Process();
+                
+            }
+            return CycleRegXValues.Select((t) => t.Item1*t.Item2).Sum().ToString();   
+        }
+
+        public static string ExecutePart2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ AdventOfCode2022_Tests/Day10Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022_Tests
+{
+    [TestClass]
+    public class Day10Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual("13140", Day10.ExecutePart1(AoCFile.ReadTestInput(10)));
+        }
+        [TestMethod]
+        public void TestPart2()
+        {
+            //Assert.AreEqual("45000", Day10.ExecutePart2(AoCFile.ReadTestInput(10)));
+        }
+        [TestMethod]
+        public void TestSimple()
+        {
+            var instr = new[] { "noop", "addx 3", "addx -5" }.Select(Day10.Instruction.Parse).ToList();
+            var p = new Day10.Processor();
+            var expects = new List<int>() { 1, 1, 1, 4, -1 };
+            
+            for (int i = 0; i < 5; i++)
+            {
+                if (i < instr.Count) p.Load(instr[i]);
+                Assert.AreEqual(expects[i], p.RegisterX);
+                p.Process();
+
+            }
+        }
+    }
+}
