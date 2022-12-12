6ea40e10f88bd1ee07396da5ac2bddef60067ff3
Erik <erikvsoderberg@gmail.com>
2022-12-12 21:52:22 +0100

    Apparently [][] arrays are somewhat faster than [,] arrays. Also cleaned up some unneeded stopwatches

--- AdventOfCode2022/Day12.cs
+++ AdventOfCode2022/Day12.cs
@@ -16,14 +16,12 @@ namespace AdventOfCode2022
             public bool visited;
             public (int, int) visitedBy;
         }
-
-        private Dictionary<(int, int), List<(int, int)>> climbableNeighbours = new();
-        public ClimbingGraph(Node[,] grid, Func<int, int, bool> neighbourCondition)
+        public ClimbingGraph(Node[][] grid, Func<int, int, bool> neighbourCondition)
         {
             this.grid = grid;
             this.neighbourCondition = neighbourCondition;
         }
-        Node[,] grid;
+        Node[][] grid;
         Func<int, int, bool> neighbourCondition;
 
         public List<(int, int)> CalculateShortestPath((int, int) start, (int, int) end)
@@ -31,35 +29,35 @@ namespace AdventOfCode2022
             Queue<(int, int)> nodes = new();
             nodes.Enqueue(start);
             // Up, Right, Down, Left
-            List<(int, int)> dirs = new() { (-1, 0), (0, 1), (1, 0), (0, -1) };
-            int rows = grid.GetLength(0);
-            int columns = grid.GetLength(1);
+            (int, int)[] dirs = new (int, int)[] { (-1, 0), (0, 1), (1, 0), (0, -1) };
+            int rows = grid.Length;
+            int columns = grid[0].Length;
             while (nodes.Count > 0)
             {
                 var node = nodes.Dequeue();
-                foreach (var dir in dirs)
+                for (int dirIx = 0; dirIx < dirs.Length; dirIx++)
                 {
-                    var neighbour = (node.Item1 + dir.Item1, node.Item2 + dir.Item2);
+                    var neighbour = (node.Item1 + dirs[dirIx].Item1, node.Item2 + dirs[dirIx].Item2);
                     if (0 <= neighbour.Item1 && neighbour.Item1 < rows
                         && 0 <= neighbour.Item2 && neighbour.Item2 < columns)
                     {
-                        if (grid[neighbour.Item1, neighbour.Item2].visited) continue;
-                        else if (neighbourCondition(grid[node.Item1, node.Item2].Height, grid[neighbour.Item1, neighbour.Item2].Height))
+                        if (grid[neighbour.Item1][neighbour.Item2].visited) continue;
+                        else if (neighbourCondition(grid[node.Item1][node.Item2].Height, grid[neighbour.Item1][neighbour.Item2].Height))
                         {
                             if (neighbour == end)
                             {
                                 List<(int, int)> path = new() { neighbour, node };
                                 while (node != start)
                                 {
-                                    node = grid[node.Item1, node.Item2].visitedBy;
+                                    node = grid[node.Item1][node.Item2].visitedBy;
                                     path.Add(node);
                                 }
                                 return path;
                             }
                             else
                             {
-                                grid[neighbour.Item1, neighbour.Item2].visited = true;
-                                grid[neighbour.Item1, neighbour.Item2].visitedBy = node;
+                                grid[neighbour.Item1][neighbour.Item2].visited = true;
+                                grid[neighbour.Item1][neighbour.Item2].visitedBy = node;
                                 nodes.Enqueue(neighbour);
                             }
                         }
@@ -77,8 +75,8 @@ namespace AdventOfCode2022
             nodes.Enqueue(start);
             // Up, Right, Down, Left
             (int, int)[] dirs = new (int, int)[]{ (-1, 0), (0, 1), (1, 0), (0, -1) };
-            int rows = grid.GetLength(0);
-            int columns = grid.GetLength(1);
+            int rows = grid.Length;
+            int columns = grid[0].Length;
             while (nodes.Count > 0)
             {
                 var node = nodes.Dequeue();
@@ -88,23 +86,23 @@ namespace AdventOfCode2022
                     if (0 <= neighbour.Item1 && neighbour.Item1 < rows
                         && 0 <= neighbour.Item2 && neighbour.Item2 < columns)
                     {
-                        if (grid[neighbour.Item1,neighbour.Item2].visited) continue;
-                        else if (neighbourCondition(grid[node.Item1, node.Item2].Height, grid[neighbour.Item1, neighbour.Item2].Height))
+                        if (grid[neighbour.Item1][neighbour.Item2].visited) continue;
+                        else if (neighbourCondition(grid[node.Item1][node.Item2].Height, grid[neighbour.Item1][neighbour.Item2].Height))
                         {
-                            if (grid[neighbour.Item1, neighbour.Item2].Height == end)
+                            if (grid[neighbour.Item1][neighbour.Item2].Height == end)
                             {
                                 List<(int, int)> path = new() { neighbour, node };
                                 while (node != start)
                                 {
-                                    node = grid[node.Item1, node.Item2].visitedBy;
+                                    node = grid[node.Item1][node.Item2].visitedBy;
                                     path.Add(node);
                                 }
                                 return path;
                             }
                             else
                             {
-                                grid[neighbour.Item1, neighbour.Item2].visited = true;
-                                grid[neighbour.Item1, neighbour.Item2].visitedBy = node;
+                                grid[neighbour.Item1][neighbour.Item2].visited = true;
+                                grid[neighbour.Item1][neighbour.Item2].visitedBy = node;
                                 nodes.Enqueue(neighbour);
                             }
                         }
@@ -119,19 +117,20 @@ namespace AdventOfCode2022
     {
 
 
-        public static (Node[,],(int,int),(int,int)) ParseHeightmap(List<string> input)
+        public static (Node[][],(int,int),(int,int)) ParseHeightmap(List<string> input)
         {
             Stopwatch w = new();
             w.Start();
             int rows = input.Count;
             int columns = input[0].Length;
-            Node[,] grid = new Node[rows, columns];
+            Node[][] grid = new Node[rows][];
 
             (int, int) start = (-1,-1);
             (int, int) end = (-1, -1);
 
             for (int row = 0; row < rows; row++)
             {
+                grid[row] = new Node[columns];
                 for (int column = 0; column < columns; column++)
                 {
                     var chr = input[row][column];
@@ -139,18 +138,18 @@ namespace AdventOfCode2022
                     {
                         case 'S':
                             start = (row, column);
-                            grid[row, column].Height = 0;
+                            grid[row][column].Height = 0;
                             break;
                         case 'E':
                             end = (row, column);
-                            grid[row, column].Height = 'z' - 'a';
+                            grid[row][column].Height = 'z' - 'a';
                             break;
                         default:
-                            grid[row, column].Height = input[row][column] - 'a';
+                            grid[row][column].Height = input[row][column] - 'a';
                             break;
                     }
-                    grid[row, column].visited = false;
-                    grid[row, column].visitedBy = (-1, -1);
+                    grid[row][column].visited = false;
+                    grid[row][column].visitedBy = (-1, -1);
                 }
             }
             w.Stop();
@@ -166,11 +165,9 @@ namespace AdventOfCode2022
             var grid = result.Item1;
             (int, int) start = result.Item2;
             (int, int) end = result.Item3;
-            Stopwatch w = new();
-            w.Start();
+
             var graph = new ClimbingGraph(grid, (height, neighbourHeight) => height+1 >= neighbourHeight);
-            w.Stop();
-            System.Console.Out.WriteLine($" parse ms {w.ElapsedMilliseconds}");
+
             var path = graph.CalculateShortestPath(start, end);
             return (path.Count -1).ToString();
         }
@@ -183,11 +180,9 @@ namespace AdventOfCode2022
             var grid = result.Item1;
             (int, int) _ = result.Item2;
             (int, int) start = result.Item3;
-            Stopwatch w = new();
-            w.Start();
+
             var graph = new ClimbingGraph(grid, (height, neighbourHeight) => height-1 <= neighbourHeight);
-            w.Stop();
-            System.Console.Out.WriteLine($" parse ms {w.ElapsedMilliseconds}");
+
             var path = graph.CalculateShortestPathToHeight(start, 0);
             return (path.Count -1).ToString();
         }
