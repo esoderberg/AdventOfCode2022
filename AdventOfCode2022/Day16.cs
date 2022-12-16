c8d3544d3e123805d255678347813155338db579
Erik <erikvsoderberg@gmail.com>
2022-12-17 00:39:31 +0100

    Solved Day 16 part 1 and maybe 2

---
+++ AdventOfCode2022/Day16.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static AdventOfCode2022.Day16;
+
+namespace AdventOfCode2022
+{
+    public class Day16 : AoCDay
+    {
+        public record Valve(string Id, int FlowRate, List<string> Neighbours)
+        {
+            public override string ToString()
+            {
+                return $"{Id}-{FlowRate}";
+            }
+        }
+
+        private static Regex parser = new Regex(@"Valve (?<id>\w\w) has flow rate=(?<flow>\d+); tunnels? leads? to valves? (?<neighbours>.*)");
+        public static Valve Parse(string line)
+        {
+            var match = parser.Match(line);
+            string id = match.Groups["id"].Value;
+            int flow = int.Parse(match.Groups["flow"].Value);
+            List<string> neighbours = match.Groups["neighbours"].Value.Split(", ").ToList();
+            return new Valve(id, flow, neighbours);
+        }
+
+        public static Dictionary<(string,string), int> CalculateShortestDistances(List<Valve> valves)
+        {
+            Dictionary<(string, string), int> distance = new();
+            var valveDict = new Dictionary<string, Valve>(valves.Select(v => new KeyValuePair<string, Valve>(v.Id, v)));
+            // Calculate distance between each valve.
+            foreach (var valve in valves)
+            {
+                HashSet<string> visited = new();
+                Queue<(Valve, int)> toVisitforLen = new();
+                toVisitforLen.Enqueue((valve, 0));
+                visited.Add(valve.Id);
+                while (toVisitforLen.Count > 0)
+                {
+                    var tmp = toVisitforLen.Dequeue();
+                    var v = tmp.Item1;
+                    var length = tmp.Item2;
+                    distance[(valve.Id, v.Id)] = length;
+                    distance[(v.Id, valve.Id)] = length;
+
+                    foreach (var neighbour in v.Neighbours)
+                    {
+                        if (!visited.Contains(neighbour))
+                        {
+                            visited.Add(neighbour);
+                            toVisitforLen.Enqueue((valveDict[neighbour], length + 1));
+                        }
+                    }
+                }
+            }
+            return distance;
+        }
+
+        public static string ExecutePart1(List<string> input)
+        {
+            var valves = input.Select(Parse).ToList();
+            var nonZeroValves = valves.Where(v => v.FlowRate > 0).ToList();
+            var valveDict = new Dictionary<string, Valve>(valves.Select(v => new KeyValuePair<string, Valve>(v.Id, v)));
+            var openValves = new List<Valve>();
+            var distance = CalculateShortestDistances(valves);
+
+            List<(int, List<(string,int)>)> completed = new();
+            Queue<(string, HashSet<string>, int, int, List<(string,int)>)> toVisit = new();
+            // Current node      , remaining nodes                     , minutes left, released pressure, path 
+            toVisit.Enqueue(("AA", new(nonZeroValves.Select(v => v.Id)),           30,                 0, new() { ("AA",30) }));
+            while(toVisit.Count > 0)
+            {
+                var p = toVisit.Dequeue();
+                if (p.Item2.Count > 0) // This is some valve we can still visit
+                {
+                    bool wentSomewhere = false;
+                    // Go through each of the remaining valves
+                    foreach (var valve in p.Item2)
+                    {
+                        var timeRequiredToGoAndOpen = distance[(valve, p.Item1)] + 1;
+                        var timeLeftAfterWalkAndOpen = p.Item3 - timeRequiredToGoAndOpen;
+                        // We must have time to walk to the node, open it and have some time afterwards to gain anything from it it.
+                        if (timeLeftAfterWalkAndOpen > 0)
+                        {
+                            wentSomewhere = true;
+                            HashSet<string> remaining = new(p.Item2);
+                            remaining.Remove(valve);
+                            var pressureReleased = p.Item4 + timeLeftAfterWalkAndOpen * valveDict[valve].FlowRate;
+                            toVisit.Enqueue((valve, remaining, timeLeftAfterWalkAndOpen, pressureReleased, new(p.Item5) { (valve,timeLeftAfterWalkAndOpen) }));
+                        }
+                        else 
+                        { 
+                        }
+
+                    }
+                    // If there was no new valve we could visit within the remaining time then this path is completed.
+                    if (!wentSomewhere)
+                        completed.Add((p.Item4,p.Item5));
+                }
+                else completed.Add((p.Item4, p.Item5));
+            }
+            completed = completed.OrderByDescending(p => p.Item1).ToList();
+            return completed.Max(p => p.Item1).ToString();
+        }
+
+        public static string ExecutePart2(List<string> input)
+        {
+            var valves = input.Select(Parse).ToList();
+            var nonZeroValves = valves.Where(v => v.FlowRate > 0).ToList();
+            var valveDict = new Dictionary<string, Valve>(valves.Select(v => new KeyValuePair<string, Valve>(v.Id, v)));
+            var openValves = new List<Valve>();
+            var distance = CalculateShortestDistances(valves);
+
+            List<(int, List<((string,string), (int,int))>)> completed = new();
+            Queue<((string me,string ele) pos, HashSet<string> remaining, (int me,int ele) time, int pressure, List<((string mpos,string epos), (int mt,int et))> path)> toVisit = new();
+            // Current node      , remaining nodes                     , minutes left, released pressure, path 
+            toVisit.Enqueue((("AA","AA"), new(nonZeroValves.Select(v => v.Id)), (26,26), 0, new() { (("AA","AA"), (26,26)) }));
+            while (toVisit.Count > 0)
+            {
+                var p = toVisit.Dequeue();
+                if (p.remaining.Count > 0) // This is some valve we can still visit
+                {
+                    bool wentSomewhere = false;
+                    // Go through each of the remaining valves
+                    var mypos = p.pos.me;
+                    var mytime = p.time.me;
+                    var elepos = p.pos.ele;
+                    var eletime = p.time.ele;
+
+                    var valvesICanVisit = p.remaining.Select<string,(string id, int time)>(n => (n, mytime - (distance[(n, mypos)] + 1))).Where(n => n.time > 0).ToList();
+                    if(valvesICanVisit.Count == 0)
+                        valvesICanVisit.Add((mypos,0));
+                    var valvesElephantCanVisit = p.remaining.Select<string, (string id, int time)>(n => (n,eletime -(distance[(n, elepos)] + 1))).Where(n => n.time > 0).ToList();
+                    if(valvesElephantCanVisit.Count == 0)
+                        valvesElephantCanVisit.Add((elepos, 0));
+
+                    foreach (var myValve in valvesICanVisit)
+                    {
+                        foreach (var eleValve in valvesElephantCanVisit)
+                        {
+                            if (myValve.id == eleValve.id) continue;
+                            wentSomewhere = true;
+                            HashSet<string> remaining = new(p.remaining);
+                            remaining.Remove(myValve.id);
+                            remaining.Remove(eleValve.id);
+                            var pressureReleased = p.pressure + myValve.time * valveDict[myValve.id].FlowRate + eleValve.time * valveDict[eleValve.id].FlowRate;
+                            toVisit.Enqueue(
+                                ((myValve.id, eleValve.id),
+                                remaining, 
+                                (myValve.time, eleValve.time),
+                                pressureReleased,
+                                new(p.path) { ((myValve.id,eleValve.id),(myValve.time, eleValve.time)) }) );
+                        }
+                    }
+
+                    // If there was no new valve we could visit within the remaining time then this path is completed.
+                    if (!wentSomewhere)
+                        completed.Add((p.pressure, p.path));
+                }
+                else completed.Add((p.pressure, p.path));
+            }
+            completed = completed.OrderByDescending(p => p.Item1).ToList();
+            return completed.Max(p => p.Item1).ToString();
+        }
+    }
+}
---
+++ AdventOfCode2022_Tests/Day16Test.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022_Tests
+{
+    [TestClass]
+    public class Day16Test
+    {
+        [TestMethod]
+        public void TestPart1()
+        {
+            Assert.AreEqual("1651", Day16.ExecutePart1(AoCFile.ReadTestInput(16)));
+        }
+
+
+
+
+        [TestMethod]
+        public void TestPart1_case2()
+        {
+            var s = """
+            Valve AA has flow rate=0; tunnels lead to valves BA
+            Valve BA has flow rate=2; tunnels lead to valves AA, CA
+            Valve CA has flow rate=4; tunnels lead to valves BA, DA
+            Valve DA has flow rate=6; tunnels lead to valves CA, EA
+            Valve EA has flow rate=8; tunnels lead to valves DA, FA
+            Valve FA has flow rate=10; tunnels lead to valves EA, GA
+            Valve GA has flow rate=12; tunnels lead to valves FA, HA
+            Valve HA has flow rate=14; tunnels lead to valves GA, IA
+            Valve IA has flow rate=16; tunnels lead to valves HA, JA
+            Valve JA has flow rate=18; tunnels lead to valves IA, KA
+            Valve KA has flow rate=20; tunnels lead to valves JA, LA
+            Valve LA has flow rate=22; tunnels lead to valves KA, MA
+            Valve MA has flow rate=24; tunnels lead to valves LA, NA
+            Valve NA has flow rate=26; tunnels lead to valves MA, OA
+            Valve OA has flow rate=28; tunnels lead to valves NA, PA
+            Valve PA has flow rate=30; tunnels lead to valves OA
+            """.Split("\r\n").ToList();
+            Assert.AreEqual("2640", Day16.ExecutePart1(s));
+        }
+
+        [TestMethod]
+        public void TestPart1_case3()
+        {
+            var s = """
+            Valve AA has flow rate=0; tunnels lead to valves BA
+            Valve BA has flow rate=1; tunnels lead to valves AA, CA
+            Valve CA has flow rate=4; tunnels lead to valves BA, DA
+            Valve DA has flow rate=9; tunnels lead to valves CA, EA
+            Valve EA has flow rate=16; tunnels lead to valves DA, FA
+            Valve FA has flow rate=25; tunnels lead to valves EA, GA
+            Valve GA has flow rate=36; tunnels lead to valves FA, HA
+            Valve HA has flow rate=49; tunnels lead to valves GA, IA
+            Valve IA has flow rate=64; tunnels lead to valves HA, JA
+            Valve JA has flow rate=81; tunnels lead to valves IA, KA
+            Valve KA has flow rate=100; tunnels lead to valves JA, LA
+            Valve LA has flow rate=121; tunnels lead to valves KA, MA
+            Valve MA has flow rate=144; tunnels lead to valves LA, NA
+            Valve NA has flow rate=169; tunnels lead to valves MA, OA
+            Valve OA has flow rate=196; tunnels lead to valves NA, PA
+            Valve PA has flow rate=225; tunnels lead to valves OA
+            """.Split("\r\n").ToList();
+            Assert.AreEqual("13468", Day16.ExecutePart1(s));
+        }
+
+        [TestMethod]
+        public void TestPart1_case4()
+        {
+            var s = """
+            Valve BA has flow rate=2; tunnels lead to valves AA, CA
+            Valve CA has flow rate=10; tunnels lead to valves BA, DA
+            Valve DA has flow rate=2; tunnels lead to valves CA, EA
+            Valve EA has flow rate=10; tunnels lead to valves DA, FA
+            Valve FA has flow rate=2; tunnels lead to valves EA, GA
+            Valve GA has flow rate=10; tunnels lead to valves FA, HA
+            Valve HA has flow rate=2; tunnels lead to valves GA, IA
+            Valve IA has flow rate=10; tunnels lead to valves HA, JA
+            Valve JA has flow rate=2; tunnels lead to valves IA, KA
+            Valve KA has flow rate=10; tunnels lead to valves JA, LA
+            Valve LA has flow rate=2; tunnels lead to valves KA, MA
+            Valve MA has flow rate=10; tunnels lead to valves LA, NA
+            Valve NA has flow rate=2; tunnels lead to valves MA, OA
+            Valve OA has flow rate=10; tunnels lead to valves NA, PA
+            Valve PA has flow rate=2; tunnels lead to valves OA, AA
+            Valve AA has flow rate=0; tunnels lead to valves BA, PA
+            """.Split("\r\n").ToList();
+            Assert.AreEqual("1288", Day16.ExecutePart1(s));
+        }
+        [TestMethod]
+        public void TestPart1_case5()
+        {
+            var s = """
+            Valve AA has flow rate=0; tunnels lead to valves AB, BB, CB
+            Valve AB has flow rate=0; tunnels lead to valves AA, AC
+            Valve AC has flow rate=0; tunnels lead to valves AB, AD
+            Valve AD has flow rate=0; tunnels lead to valves AC, AE
+            Valve AE has flow rate=0; tunnels lead to valves AD, AF
+            Valve AF has flow rate=0; tunnels lead to valves AE, AG
+            Valve AG has flow rate=0; tunnels lead to valves AF, AH
+            Valve AH has flow rate=0; tunnels lead to valves AG, AI
+            Valve AI has flow rate=0; tunnels lead to valves AH, AJ
+            Valve AJ has flow rate=0; tunnels lead to valves AI, AK
+            Valve AK has flow rate=100; tunnels lead to valves AJ, AW, AX, AY, AZ
+            Valve AW has flow rate=10; tunnels lead to valves AK
+            Valve AX has flow rate=10; tunnels lead to valves AK
+            Valve AY has flow rate=10; tunnels lead to valves AK
+            Valve AZ has flow rate=10; tunnels lead to valves AK
+            Valve BB has flow rate=0; tunnels lead to valves AA, BC
+            Valve BC has flow rate=0; tunnels lead to valves BB, BD
+            Valve BD has flow rate=0; tunnels lead to valves BC, BE
+            Valve BE has flow rate=0; tunnels lead to valves BD, BF
+            Valve BF has flow rate=0; tunnels lead to valves BE, BG
+            Valve BG has flow rate=0; tunnels lead to valves BF, BH
+            Valve BH has flow rate=0; tunnels lead to valves BG, BI
+            Valve BI has flow rate=0; tunnels lead to valves BH, BJ
+            Valve BJ has flow rate=0; tunnels lead to valves BI, BK
+            Valve BK has flow rate=100; tunnels lead to valves BJ, BW, BX, BY, BZ
+            Valve BW has flow rate=10; tunnels lead to valves BK
+            Valve BX has flow rate=10; tunnels lead to valves BK
+            Valve BY has flow rate=10; tunnels lead to valves BK
+            Valve BZ has flow rate=10; tunnels lead to valves BK
+            Valve CB has flow rate=0; tunnels lead to valves AA, CC
+            Valve CC has flow rate=0; tunnels lead to valves CB, CD
+            Valve CD has flow rate=0; tunnels lead to valves CC, CE
+            Valve CE has flow rate=0; tunnels lead to valves CD, CF
+            Valve CF has flow rate=0; tunnels lead to valves CE, CG
+            Valve CG has flow rate=0; tunnels lead to valves CF, CH
+            Valve CH has flow rate=0; tunnels lead to valves CG, CI
+            Valve CI has flow rate=0; tunnels lead to valves CH, CJ
+            Valve CJ has flow rate=0; tunnels lead to valves CI, CK
+            Valve CK has flow rate=100; tunnels lead to valves CJ, CW, CX, CY, CZ
+            Valve CW has flow rate=10; tunnels lead to valves CK
+            Valve CX has flow rate=10; tunnels lead to valves CK
+            Valve CY has flow rate=10; tunnels lead to valves CK
+            Valve CZ has flow rate=10; tunnels lead to valves CK
+            """.Split("\r\n").ToList();
+            Assert.AreEqual("2400", Day16.ExecutePart1(s));
+        }
+
+        [TestMethod]
+        public void TestPart2()
+        {
+            Assert.AreEqual("1707", Day16.ExecutePart2(AoCFile.ReadTestInput(16)));
+        }
+    }
+}
